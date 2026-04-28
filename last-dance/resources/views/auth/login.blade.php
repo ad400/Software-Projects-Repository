d1972f84eca2b4c8693f4eb21ac7a3484dc73dfa
Zaid <adamzaid.maroc@gmail.com>
2026-04-28 16:41:27 +0100

    Set character illustrations as transparent background images in sliding forms for better aesthetics

--- last-dance/resources/views/auth/login.blade.php
+++ last-dance/resources/views/auth/login.blade.php
@@ -242,8 +242,14 @@
         }
 
         .illustration-img {
-            width: 200px;
-            margin-bottom: 20px;
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+            opacity: 0.2;
+            z-index: -1;
         }
     </style>
 </head>
@@ -308,14 +314,14 @@
     <div class="overlay-container">
         <div class="overlay">
             <div class="overlay-panel overlay-left">
-                <h1>Welcome Back!</h1>
                 <img src="{{ asset('storage/images/flymen.png') }}" class="illustration-img" alt="">
+                <h1>Welcome Back!</h1>
                 <p>To keep connected with us please login with your personal info</p>
                 <button class="ghost" id="signIn">Sign In</button>
             </div>
             <div class="overlay-panel overlay-right">
-                <h1>Hello, Friend!</h1>
                 <img src="{{ asset('storage/images/flywmn.png') }}" class="illustration-img" alt="">
+                <h1>Hello, Friend!</h1>
                 <p>Enter your personal details and start journey with us</p>
                 <button class="ghost" id="signUp">Sign Up</button>
             </div>
